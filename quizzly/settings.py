e87233269b83961bc6ea96455acc3b57e575995a
Dar3y1997 <daramolashalom2018@gmail.com>
2025-09-01 14:56:11 +0100

    new quiz version

--- quizzly/settings.py
+++ quizzly/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r@lah3g@s&x#wleia_dg#2p_yntc*l8_m0^&m)e$_utu=fi2tv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["quizzly-77kn.onrender.com", "127.0.0.1", "localhost"]
 
